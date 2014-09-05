55c83901de52344deb2a5968abfd9d4b0c344fe0
Cherepenko Paul <cherrypipka@gmail.com>
2014-09-05 12:39:44 +0300

    adyax test intermediate

--- sites/all/modules/adyax_test/plugins/content_types/adyaxtest-full.tpl.php
+++ sites/all/modules/adyax_test/plugins/content_types/adyaxtest-full.tpl.php
@@ -1,8 +1,7 @@
-<style>
-  <?php //print 'div.pane-adyaxtest {border: ' . $border_style . 'px solid gray!important; border-radius: 10px!important;}'; ?>
-</style>
-<?php foreach($nodes as $node): ?>
-  <div class="adyaxtest">
-    <?php print $node; ?>
-  </div>
-<?php endforeach; ?>
+<div <?php print $attributes; ?>>
+  <?php foreach($nodes as $node): ?>
+    <div class="adyaxtest">
+      <?php print $node; ?>
+    </div>
+  <?php endforeach; ?>
+</div>
